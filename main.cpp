60c1e6628c0fc506c276aa8b104dca275cefb803
Alice (RekhalovAS) <rekhalovdrusya52@gmail.com>
2022-03-13 18:49:01 +0300

    code:добавил разность

--- main.cpp
+++ main.cpp
@@ -7,5 +7,6 @@ int main()
    cout << "Enter A and B: ";
 int a, b;
 cin >> a >> b;
-cout << "A + B = " << a + b << '\n';
+cout << "A + B = " << a + b << '\n'
+     << "A - B = " << a - b << '\n';
 }
